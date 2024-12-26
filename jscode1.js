6ad99f6edd9d0b2a741886d84b3d9cde476749e2
Baasandash0307 <bsskaa.kai123@gmail.com>
2024-12-26 21:42:01 +0800

    Add files via upload

--- jscode1.js
+++ jscode1.js
@@ -1,73 +1,170 @@
+// let weekNumber = 1;
+// let weekName;
+
+// if(weekNumber == 1){
+//     weekName = 'davaa';
+// }
+// else if (weekNumber == 2){
+//     weekName = 'myagamr'
+// }
+// else if (weekNumber == 3){
+//     weekName = 'lhagva'
+// }
+// else if (weekNumber == 4){
+//     weekName = 'purev'
+// }
+// else if (weekNumber == 5){
+//     weekName = 'baasan'
+// }
+// else if (weekNumber == 6){
+//     weekName = 'byamba'
+// }
+// else if (weekNumber == 7){
+//     weekName = 'nyam'
+// }
+// console.log(weekName);
 
 
 
-// 1-100 hurtelh sondgoi too
+/////////////
 
 
-for(var i = 0; i < 100 ; i++)
-    if(i % 2 == 1){
-        console.log('sondgoi too', i);
+
+    // let gender = 'male';
+    // let age = 16;
+
+    // if(gender == 'male' && age >= 18){
+    //     console.log('nasand hursen eregtei');
+    // }
+    // else{
+    //     console.log('nasand hureegu eregtei')
+    // }
+
+
+
+
+//////////////////
+
+
+    
+
+// let day = 3;
+// let dayName;
+
+// switch (day) {
+//     case 1:
+//         dayName = 'monday'
+//         break;
+//         case 2:
+//             dayName = 'tuesday'
+//             break;
+//             case 3:
+//                 dayName = 'wednesday'
+//                 break;
+//                 case 4:
+//                     dayName = 'thursday'
+//                     break;
+//                     case 5:
+//                         dayName = 'friday'
+//                         break;
+// }
+// console.log(dayName)
+
+
+
+
+
+///////////////
+
+
+
+
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+
+
+////////////////
+
+
+
+
+// let sum = 0;
+// for(let i = 1; i <= 10; i++){
+//     if(i % 3 !==0)
+//         continue;
+//     sum += i;
+// }
+// console.log(sum);
+
+
+
+///////////////////
+
+
+
+
+// for(let i = 1; i <= 100; i++){
+//     if(i % 3===0 && i % 5===0){
+//         console.log(i);
+//         break;
+//     }
+// }
+
+
+////////////////////
+
+
+
+// for(let i = 100; i > 0; i--){
+//     if(i % 3===0 && i % 5===0){
+//         console.log(i);
+//         break;
+//     }
+// }
+
+
+
+
+/////////////
+
+
+// var x = 13;
+// var result;
+// for (var i = 2; i < x; i++) {
+//     result = x % i
+//     //  console.log(i, result)
+//     if (result == 0) {
+//         console.log(false)
+//     }
+//     else {
+//         console.log(true)
+//     }
+// }
+
+
+
+
+// for(i = 1; i <= 100; i++){
+//     if(i % 2==0){
+//         console.log(i  +'bol tegsh ');
+//     }
+// }
+
+
+
+
+
+
+let n = 4;
+let isPrimeNumber = true;
+
+for(i = 2; i < n; i++){
+    if(n % i==0){
+        isPrimeNumber = false
     }
-
-
-
-
-
-    // 100 hurtelh toonii niilber
-
-
-var sum = 0;
-for(var i = 1; i <= 100; i++){
-    sum = sum + i;
-}    
-    console.log('niilber', sum);
-
-
-
-
-
-    // 100 hurtelh toonii tegsh toonii urjver
-
-var urjver = 1;
-for(var i = 1; i <= 100; i++){
-    if(i % 2 == 0)
-urjver = urjver * i;
 }
-console.log('urjver', urjver)
-
-
-
-
-
-
-
-
-
-function AnhniiTooOloh(too){
-    if(too <= 1){
-        return false
-    }
-    for(var i = 2 ; i < too ; i++){
-        if(too % i === 0){
-            return false
-        }
-    }
-    return true
-}
-
-
-
-for(var i = 2 ; i < 100 ; i++){
-    if(AnhniiTooOloh(i)){
-        console.log("anhnii"+i);
-    }
-}
-
-
-var number = 4
-
-if(AnhniiTooOloh(number) === true){
-    console.log("Anhni too")
-}else{
-    console.log("bish")
-}
+console.log(isPrimeNumber);
